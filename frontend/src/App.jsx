8ed077d5c273594c0fd764e0029b900d0f7db5de
arjun <arjunanoop101@gmail.com>
2024-09-08 16:33:15 +0530

    cart page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,7 +16,7 @@ const App = () => {
       <Navbar setShowLogin={setShowLogin}/>
       <Routes>
         <Route path='/' element={<Home/>}/>
-        <Route path='/card' element={<Cart/>}/>
+        <Route path='/cart' element={<Cart/>}/>
         <Route path='/order' element={<PlaceOrder/>}/>
       </Routes>
       
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,12 +1,14 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import './Navbar.css'
 import {assets} from '../../assets/assets'
 import {Link} from 'react-router-dom'
+import { StoreContext } from '../../context/StoreContext'
 const Navbar = ({setShowLogin}) => {
     const [menu,setMenu] = useState('Home')
+    const {getTotalCartAmount} = useContext(StoreContext)
   return (
     <div className='navbar'>
-      <h2 className='logo'>Healthify </h2>
+     <Link to="/"><h2 className='logo'>Healthify </h2></Link> 
       <ul className="navbar-menu">
         <Link to='/' onClick={()=>setMenu('Home')} className={menu==="Home"?"active":""}>Home</Link>
         <a href="#explore-menu"onClick={()=>setMenu('Menu')} className={menu==="Menu"?"active":""}>Menu</a>
@@ -16,8 +18,8 @@ const Navbar = ({setShowLogin}) => {
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
         <div className="navbar-basket-icon">
-            <img src={assets.basket_icon} alt="" />
-            <div className="dot"></div>
+           <Link to='/cart'><img src={assets.basket_icon} alt="" /></Link> 
+            <div className={getTotalCartAmount()===0?"":"dot"}></div>
         </div>
         <button onClick={()=>{setShowLogin(true)}}>Sign In</button>
       </div>
